5c7ec10f4e0f44b4e105503e5cb79b503b919bb8
Dias Rakhman <rahmanovdias13@gmail.com>
2024-10-08 11:06:54 +0500

    first commit

---
+++ src/main/java/com/example/postify/PostifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.postify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostifyApplication.class, args);
+    }
+
+}
