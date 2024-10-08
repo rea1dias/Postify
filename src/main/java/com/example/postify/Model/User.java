a510f0e806f109d285c6e2bb94029195a072ea12
Dias Rakhman <rahmanovdias13@gmail.com>
2024-10-08 11:31:33 +0500

    База данных была подключена

---
+++ src/main/java/com/example/postify/Model/User.java
@@ -0,0 +1,97 @@
+package com.example.postify.Model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String bio;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    public User() {}
+
+    public User(String name, String email, String password, String bio, String avatarUrl, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+        this.avatarUrl = avatarUrl;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
